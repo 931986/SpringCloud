29b41af813cc1d42b4eba6abfb6c12a432923777
sunyifan <57336928@qq.com>
2021-11-24 17:07:30 +0800

    config

--- cloud-config-01/src/main/java/com/SpringCloud/Config01.java
+++ cloud-config-01/src/main/java/com/SpringCloud/Config01.java
@@ -1,4 +1,16 @@
 package com.SpringCloud;
 
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableConfigServer
 public class Config01 {
+    public static void main(String[] args) {
+        SpringApplication.run(Config01.class,args);
+    }
 }
